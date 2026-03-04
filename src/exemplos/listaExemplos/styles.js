009a504a121125e6f585c71f4cd605e8b6848e7e
joaoromeira-etec <joaopedrodiasromeira456@gmail.com>
2026-03-04 19:30:10 -0300

    styles update

--- src/exemplos/listaExemplos/styles.js
+++ src/exemplos/listaExemplos/styles.js
@@ -1,28 +1,32 @@
 import { StyleSheet } from 'react-native'; 
 import { RFPercentage } from "react-native-responsive-fontsize";
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1, 
-        padding: RFPercentage(1), 
-        alignItems: 'center', 
-        gap: RFPercentage(1),
-    }, 
-    titulo: {
-        fontSize: RFPercentage(3),
-        fontWeight: 'bold', 
-        marginVertical: RFPercentage(2), 
-    }, 
-    botao: {
-        borderWidth: RFPercentage(0.3), 
-        padding: RFPercentage(1), 
-        width: '60%', 
-        alignItems: 'center', 
-        borderRadius: RFPercentage(1),
-    }, 
-    txtBotao: {
-        fontSize: RFPercentage(2),
+const styles = StyleSheet.create(
+    {
+        container: { 
+            flex: 1,
+            padding: RFPercentage(1),   
+            alignItems: 'center',
+            gap: RFPercentage(3),
+        },
+        titulo: {
+            fontSize: RFPercentage(4.5),
+            color: '#650C0C',
+            fontWeight: 'bold',
+            marginVertical: RFPercentage(1.75),
+        }, 
+        botao: {
+            borderWidth: RFPercentage(0.3),
+            padding: RFPercentage(0.5),
+            width: '80%',
+            alignItems: 'center',
+            borderRadius: RFPercentage(1)
+        }, 
+        txtBotao: {
+            fontWeight: 'bold',
+            fontSize: RFPercentage(2), 
+        },
     }
-});
+);  
 
 export default styles;
